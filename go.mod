d7471a84ae9b22a9f4291dc9e9a9293e299c6c7a
midzu6 <yotsume52@gmail.com>
2026-03-31 23:49:21 +0300

    add basic main

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module gochatapp
 
 go 1.26
+
+require github.com/gorilla/websocket v1.5.3
+
+require github.com/google/uuid v1.6.0 // indirect
---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"sync"
+
+	"github.com/google/uuid"
+	"github.com/gorilla/websocket"
+)
+
+var (
+	WSPort = ":8081"
+)
+
+type Client struct {
+	ID   string
+	mu   *sync.RWMutex
+	conn *websocket.Conn
+}
+
+type Server struct {
+	mu      *sync.RWMutex
+	clients map[*Client]bool
+}
+
+func NewServer() *Server {
+	return &Server{
+		mu:      new(sync.RWMutex),
+		clients: make(map[*Client]bool),
+	}
+}
+
+func NewClient(conn *websocket.Conn) *Client {
+	ID := uuid.New().String()
+	return &Client{
+		ID:   ID,
+		mu:   new(sync.RWMutex),
+		conn: conn,
+	}
+}
+
+func (s *Server) handleWs(w http.ResponseWriter, r *http.Request) {
+	upgrader := websocket.Upgrader{
+		WriteBufferSize: 512,
+		ReadBufferSize:  512,
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Printf("error upgrade http to websocket, err %v\r", err)
+		return
+	}
+
+	client := NewClient(conn)
+
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.clients[client] = true
+}
+
+func main() {
+
+	server := NewServer()
+
+	http.HandleFunc("/", server.handleWs)
+
+	log.Fatal(http.ListenAndServe(WSPort, nil))
+
+}
