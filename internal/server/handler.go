2d1796092349d0ebb69a1a1849fbbae252cd94d4
midzuズTokyo <yotsume52@gmail.com>
2026-04-01 23:17:25 +0300

    добавлен handler (создает вебсокет конекшн и добавляет клиента серверу)

---
+++ internal/server/handler.go
@@ -0,0 +1,28 @@
+package server
+
+import (
+	"errors"
+	c "gochatapp/internal/client"
+	ws "gochatapp/pkg/websocket"
+	"log"
+	"net/http"
+)
+
+func (s *Server) HandleWS(w http.ResponseWriter, r *http.Request) {
+	conn, err := ws.Upgrade(w, r)
+	if err != nil {
+		if errors.Is(err, ws.ErrFailUpgrade) {
+			log.Printf("fail upgrade http to websocket, err %v\n", err)
+			return
+		}
+		log.Printf("unexpected error, err %v\n", err)
+		return
+	}
+	defer conn.Close()
+
+	client := c.NewClient(conn)
+	s.joinCh <- client
+
+	// add client's methods
+
+}
