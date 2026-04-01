6df9fa069d7956abc0881abce9596fa280d6d644
midzuズTokyo <yotsume52@gmail.com>
2026-04-02 02:06:20 +0300

    change structure of Message type

--- internal/client/message.go
+++ internal/client/message.go
@@ -1,10 +1,5 @@
 package client
 
 type Message struct {
-	ClientId string
-	Data     string
-}
-
-func NewMessage(id string, data string) *Message {
-	return &Message{ClientId: id, Data: data}
+	Text string
 }
